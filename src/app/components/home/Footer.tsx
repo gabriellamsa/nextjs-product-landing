deb8bc702caa27207ad56985357b173d126c4c78
gabriellamsa <gabii.marcelly@gmail.com>
2025-04-08 16:14:35 -0500

    debug: fix Next.js config and resolve unused icon import in Footer component

--- src/app/components/home/Footer.tsx
+++ src/app/components/home/Footer.tsx
@@ -1,4 +1,4 @@
-import { FaDribbble, FaFacebook, FaYoutube } from "react-icons/fa";
+import { FaFacebook, FaYoutube } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
 export const Footer = () => {
